56e0e9ab20fc6f07319b2f42cd67cb51ea4b0938
ecolon3 <80561805+ecolon3@users.noreply.github.com>
2021-07-05 08:46:56 -0800

    Updating html, css, and js with dummy features and to show day/time

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,58 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  moscow: {
-    temp: -5,
-    humidity: 20,
-  },
-};
-let cityEntered = prompt("Enter a city");
-let city = cityEntered.trim().toLowerCase();
-if (city.length > 0 && city in weather) {
-  let fahrenheit = weather[city].temp * 1.8 + 32.0;
-  fahrenheit = Number.parseFloat(fahrenheit).toFixed(1);
-  let message = `It is currently ${
-    weather[city].temp
-  }°C(${fahrenheit}°F) in ${cityEntered.trim()} with a humidity of ${
-    weather[city].humidity
-  }%`;
-  alert(message);
-} else if (city.length > 0 && city in weather === false) {
-  let errorMessage = `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city.replace(
-    " ",
-    "+"
-  )}`;
-  alert(errorMessage);
-} else {
-  let blankMessage = `Sorry, you need to enter a city name to get the weather.`;
-  alert(blankMessage);
+//Display current time and day
+function formatMinutes() {
+  let now = new Date();
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return minutes;
 }
+function getTime() {
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = formatMinutes();
+  let time = `${hours}:${minutes}`;
+  return time;
+}
+function getDay() {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let now = new Date();
+  let day = days[now.getDay()];
+  return day;
+}
+let timeDayDisplayed = document.querySelector("#current-time-day");
+timeDayDisplayed.innerHTML = `${getTime()} ${getDay()}`;
+
+//Change current city name to match city submitted in search form
+function updateCityName(event) {
+  event.preventDefault();
+  let city = document.querySelector("#another-city");
+  console.log(city.value);
+  let currentCity = document.querySelector("#current-city-name");
+  currentCity.innerHTML = city.value;
+}
+let citySearch = document.querySelector("#city-search");
+citySearch.addEventListener("submit", updateCityName);
+
+//Toggle current temperature to Celsius or Fahrenheit
+function changeToC() {
+  let degreesC = document.querySelector("#current-degrees");
+  degreesC.innerHTML = "24";
+}
+function changeToF() {
+  let degreesF = document.querySelector("#current-degrees");
+  degreesF.innerHTML = "75";
+}
+let degreesC = document.querySelector("#degrees-c");
+degreesC.addEventListener("click", changeToC);
+
+let degreesF = document.querySelector("#degrees-f");
+degreesF.addEventListener("click", changeToF);
