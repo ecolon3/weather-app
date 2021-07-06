0e7c1d861a1444e60dd8ece20b205c4957d30cdb
ecolon3 <80561805+ecolon3@users.noreply.github.com>
2021-07-06 08:12:16 -0800

    Adding current temperature functionality by city name and GPS

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ function getDay() {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
   let now = new Date();
   let day = days[now.getDay()];
@@ -31,28 +31,52 @@ function getDay() {
 let timeDayDisplayed = document.querySelector("#current-time-day");
 timeDayDisplayed.innerHTML = `${getTime()} ${getDay()}`;
 
-//Change current city name to match city submitted in search form
+//Change current city name and temp to match city submitted in search form
 function updateCityName(event) {
   event.preventDefault();
   let city = document.querySelector("#another-city");
-  console.log(city.value);
   let currentCity = document.querySelector("#current-city-name");
   currentCity.innerHTML = city.value;
 }
+function updateTemperature(response) {
+  let temp = Math.round(response.data.main.temp);
+  let currentDegrees = document.querySelector("#current-degrees");
+  currentDegrees.innerHTML = temp;
+}
+
+function getTempByCity() {
+  let apiKey = "68c9ee99b56155d1827a40287a70216c";
+  let currentCity = document.querySelector("#current-city-name");
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity.innerHTML}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(updateTemperature);
+}
+function changeNameToGPS(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let gPSCoords = `Latitude ${latitude}, Longitude ${longitude}`;
+  let location = document.querySelector("#current-city-name");
+  location.innerHTML = gPSCoords;
+}
+function getTempByGPS(position) {
+  let apiKey = "68c9ee99b56155d1827a40287a70216c";
+  let units = "imperial";
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(updateTemperature);
+}
+
+function getGPS() {
+  navigator.geolocation.getCurrentPosition(getTempByGPS);
+  navigator.geolocation.getCurrentPosition(changeNameToGPS);
+}
+
+getTempByCity();
 let citySearch = document.querySelector("#city-search");
 citySearch.addEventListener("submit", updateCityName);
+citySearch.addEventListener("submit", getTempByCity);
 
-//Toggle current temperature to Celsius or Fahrenheit
-function changeToC() {
-  let degrees = document.querySelector("#current-degrees");
-  degrees.innerHTML = "24";
-}
-function changeToF() {
-  let degrees = document.querySelector("#current-degrees");
-  degrees.innerHTML = "75";
-}
-let degreesC = document.querySelector("#degrees-c");
-degreesC.addEventListener("click", changeToC);
-
-let degreesF = document.querySelector("#degrees-f");
-degreesF.addEventListener("click", changeToF);
+//Adding a listener for the "Weather where you are now" button
+let gpsButton = document.querySelector("#gps-button");
+gpsButton.addEventListener("click", getGPS);
